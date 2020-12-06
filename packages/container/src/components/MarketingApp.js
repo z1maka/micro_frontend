c3e0a4c6170ab07045dbcc87228f30c4f2de1c39
Z1maka <zhenya.masnyy@gmail.com>
2020-12-06 19:04:35 +0200

    Fix path for mount function in container

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import { mount } from "packages/marketing/MarketingApp";
+import { mount } from "marketing/MarketingApp";
 
 export default () => {
   const ref = useRef(null);
